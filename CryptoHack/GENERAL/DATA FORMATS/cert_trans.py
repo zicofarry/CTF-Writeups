7484b46d876869b10be5891d25065a3cb0806601
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-07-31 04:55:39 +0700

    Add files via remote

--- CryptoHack/GENERAL/DATA FORMATS/cert_trans.py
+++ CryptoHack/GENERAL/DATA FORMATS/cert_trans.py
@@ -1,48 +1,24 @@
-# from cryptography.hazmat.primitives import serialization
-# from cryptography.hazmat.backends import default_backend
-# from cryptography.hazmat.primitives.asymmetric import rsa
-# import binascii
-# -----------------
-from Crypto.PublicKey import RSA
-from sympy import factorint
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+import binascii
 
-with open("transparency.pem", "r") as f:
-    key = RSA.import_key(f.read())
+with open('transparency.pem', 'rb') as f:
+    pubkey = serialization.load_pem_public_key(f.read(), backend= default_backend())
 
-n = key.n
-e = key.e
+numbers = pubkey.public_numbers()
+n = numbers.n
+e = numbers.e
 
-print(f"n = {n}")
-print(f"e = {e}")
-ciphertext = 123456789  # contoh, harus kamu ganti dengan ciphertext asli
+print(f"modulus (n): {n}")
+print(f"exponent (e): {e}")
 
-for i in range(2**8, 2**64):
-    if pow(i, e, n) == ciphertext:
-        print(f"Flag = {i.to_bytes((i.bit_length() + 7) // 8, 'big')}")
-        break
+plaintext = b"crypto{"
+plaintext_int = int.from_bytes(plaintext, byteorder='big')
 
-factors = factorint(n)
-print(factors)
+# enkripsi: c = m^e mod n
+ciphertext = pow(plaintext_int, e, n)
 
-
-# ------------------
-
-# with open('transparency.pem', 'rb') as f:
-#     pubkey = serialization.load_pem_public_key(f.read(), backend= default_backend())
-
-# numbers = pubkey.public_numbers()
-# n = numbers.n
-# e = numbers.e
-
-# print(f"modulus (n): {n}")
-# print(f"exponent (e): {e}")
-
-# plaintext = b"crypto{"
-# plaintext_int = int.from_bytes(plaintext, byteorder='big')
-
-# # enkripsi: c = m^e mod n
-# ciphertext = pow(plaintext_int, e, n)
-
-# print("plainttext int:", plaintext_int)
-# print("ciphertext (decimal):", ciphertext)
-# print("ciphertext (hex)", hex(ciphertext))
+print("plainttext int:", plaintext_int)
+print("ciphertext (decimal):", ciphertext)
+print("ciphertext (hex)", hex(ciphertext))
---
+++ CryptoHack/SYMMETRIC CIPHERS/HOW AES WORKS/README.md
@@ -0,0 +1,34 @@
+# Keyed Permutations
+```
+crypto{bijection}
+```
+
+# Resisting Bruteforce
+```
+crypto{biclique}
+```
+
+# Structure of AES
+```
+
+```
+
+# Round Keys
+```
+crypto{r0undk3y}
+```
+
+# Confusion through Substitution
+```
+crypto{l1n34rly}
+```
+
+# Diffusion through Permutation
+```
+
+```
+
+# Bringing It All Together
+```
+
+```
